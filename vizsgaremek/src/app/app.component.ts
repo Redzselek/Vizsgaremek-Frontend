0e128703195a9faaca1d4c0a336b21312940a86e
Frigyes Elemér <125876771+drt09@users.noreply.github.com>
2025-02-17 10:42:46 +0100

    Upgraded footer + fontawesome component

--- vizsgaremek/src/app/app.component.ts
+++ vizsgaremek/src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, RouterLink],
+  imports: [RouterOutlet, RouterLink, FontAwesomeModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
