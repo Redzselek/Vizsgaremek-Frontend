3048afb891f6df92a5bac6b932a0e869f2d6df9c
Frigyes Elemér <125876771+drt09@users.noreply.github.com>
2025-02-24 17:44:38 +0100

    Login build #19

--- vizsgaremek/dist/vizsgaremek/server/angular-app-manifest.mjs
+++ vizsgaremek/dist/vizsgaremek/server/angular-app-manifest.mjs
@@ -32,8 +32,8 @@ export default {
 ],
   entryPointToBrowserMapping: undefined,
   assets: {
-    'index.csr.html': {size: 2275, hash: 'eeeb742abd831b9e3d7332369918c4d2da82c180e3f753e4f494ac115f7baebd', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 1451, hash: '42cbc74a8a894b7a5ea369a8be90874616bf826e77308ed2a8f67adab96c95d3', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'index.csr.html': {size: 2275, hash: '797c204d5d3facd0b656d3567e27e91f3a3d7d2a57dc20dd4631a7844400af01', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 1451, hash: '7b5b00f6df023f1aca5eeb1ac759ae648c1841f26e2a7fd2effe3766f678c22c', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
     'styles-MECZKUYO.css': {size: 1213, hash: 'JXC4EHhpbiE', text: () => import('./assets-chunks/styles-MECZKUYO_css.mjs').then(m => m.default)}
   },
 };
--- vizsgaremek/src/app/app.component.ts
+++ vizsgaremek/src/app/app.component.ts
@@ -2,14 +2,38 @@ import { HttpClientModule } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { NavBarComponent } from "./nav-bar/nav-bar.component";
+import { DataService } from './data.service';
+import { HttpClient } from '@angular/common/http';
+import { Emitters } from './emitters/emitters';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, FontAwesomeModule, HttpClientModule, NavBarComponent],
+  imports: [RouterOutlet, FontAwesomeModule, HttpClientModule, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'vizsgaremek';
+  constructor(private dataservice: DataService, private http: HttpClient) { }
+    authenticated = false;
+  
+    url = "https://egyedirobi.moriczcloud.hu/vizsga/logout"
+    
+    ngOnInit(): void {
+      Emitters.authEmitter.subscribe(
+        (auth: boolean) => {
+          this.authenticated = auth;
+        }
+      );
+    }
+  
+    logout(){
+      console.log("logging out");
+      this.http.post<any>(this.url, {}, { withCredentials: true }).subscribe(
+        response => {
+          localStorage.removeItem('logged');
+          this.dataservice.move_to('/login');
+        }
+      )
+    }
 }
