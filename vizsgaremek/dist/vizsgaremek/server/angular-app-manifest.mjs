6421c57630828d8cfec3e50453bc114c8f9bb924
Egyedi Róbert <egyedirobika@gmail.com>
2025-03-24 16:59:50 +0100

    Hiba javítás #3
    Téves volt a value az opcióknál

--- vizsgaremek/dist/vizsgaremek/server/angular-app-manifest.mjs
+++ vizsgaremek/dist/vizsgaremek/server/angular-app-manifest.mjs
@@ -49,8 +49,8 @@ export default {
 ],
   entryPointToBrowserMapping: undefined,
   assets: {
-    'index.csr.html': {size: 8292, hash: '651637839c9cf19a6e7bd9340de2375c977867b8aff9ac2267494d1fad9c9bc3', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 7468, hash: '4d6f1631b42e9bf0e1970efffc2062294e4708d1edfe1ad459e0f6eb89f7b14b', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'index.csr.html': {size: 8292, hash: '551da74c4ad97176ad8b55e0c66b1dde6ed19c73bbd7a8c81ffa3da0708b6ab8', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 7468, hash: 'da78b15586d1dd9a094c4c1aee3f2507a808d80d8473ead746e44f3e6a00c0e8', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
     'styles-REVB3AVR.css': {size: 1213, hash: '5Fi7DFfxITE', text: () => import('./assets-chunks/styles-REVB3AVR_css.mjs').then(m => m.default)}
   },
 };
