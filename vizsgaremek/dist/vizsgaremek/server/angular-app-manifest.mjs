fd8558b9df4cb86cfdf7a6eddebb8bff8d28720b
Frigyes Elemér <125876771+drt09@users.noreply.github.com>
2025-04-07 18:44:04 +0200

    HttpRequest endpoint FIX
    - Build

--- vizsgaremek/dist/vizsgaremek/server/angular-app-manifest.mjs
+++ vizsgaremek/dist/vizsgaremek/server/angular-app-manifest.mjs
@@ -49,8 +49,8 @@ export default {
 ],
   entryPointToBrowserMapping: undefined,
   assets: {
-    'index.csr.html': {size: 8418, hash: 'bbcf5d752ef89902b9f3a91e38bc1b82068567db5f343cf47bdac3b414cb8455', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 7594, hash: '6b660c76afbfd8193643da7f3189e86011a4779196708c4718e41ea1f719792e', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'index.csr.html': {size: 8418, hash: 'fd7b886512db98f9984d91538d3670d5f4514c2e5a0ce95301472fd7d19bf2c5', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 7594, hash: '48d5fb92c83564890703c44a0421014890c22519a019f455cb9b467d6a545518', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
     'styles-REVB3AVR.css': {size: 1213, hash: '5Fi7DFfxITE', text: () => import('./assets-chunks/styles-REVB3AVR_css.mjs').then(m => m.default)}
   },
 };
